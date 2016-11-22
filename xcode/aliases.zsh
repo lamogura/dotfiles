1b77e35e313ace545ef2e1d96f6ef9824a89d0b9
Justin Kovalchuk <xentropic@gmail.com>
2016-11-22 14:17:34 +0900

    add pod_reboot alias

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,1 +1,2 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
+alias pod_reboot="rm -rf ~/Library/Caches/CocoaPods; rm -rf Pods; rm -rf ~/Library/Developer/Xcode/DerivedData/*; pod deintegrate; pod setup; pod install;"
